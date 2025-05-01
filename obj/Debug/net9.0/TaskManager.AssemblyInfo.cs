dcd02b34d6780d26ba6de6e38e815484562902cb
MattiaToscanelli <mattia.toscanelli0325@gmail.com>
2025-05-01 21:08:48 +0200

    Removed bootstrap folder

--- obj/Debug/net9.0/TaskManager.AssemblyInfo.cs
+++ obj/Debug/net9.0/TaskManager.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92e321302634efbab4b4c277260765a8ed075b0b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5497b7df9d545ab5a1ef5591876a60b21ed3d667")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

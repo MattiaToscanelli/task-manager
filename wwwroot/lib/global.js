c8a37ba8dbf06c92f56f24b228f7a2fc73d4df62
MattiaToscanelli <mattia.toscanelli0325@gmail.com>
2025-05-13 22:38:22 +0200

    fix: fix collapse mobile navbar

--- wwwroot/lib/global.js
+++ wwwroot/lib/global.js
@@ -1,5 +1,12 @@
-function closeModal() {
-    var myModalEl = document.getElementById('addBoardModal');
+function closeModal(name) {
+    var myModalEl = document.getElementById(name);
     var modal = bootstrap.Modal.getInstance(myModalEl) || new bootstrap.Modal(myModalEl);
     modal.hide();
+}
+
+function collapseNavbar() {
+    var navbar = document.getElementById('navbarNav');
+    if (navbar.classList.contains('show')) {
+        new bootstrap.Collapse(navbar).hide();
+    }
 }
